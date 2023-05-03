07ca300e157b184863209c1b63e6ec9a9b624283
puffvayne <128754474+puffvayne@users.noreply.github.com>
2023-05-04 01:40:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -255,5 +255,5 @@ async def view_profile(request: Request):
 
 
 if __name__ == '__main__':
-    # uvicorn.run('main:app', host='0.0.0.0', port=8080)
-    uvicorn.run('main:app', host='localhost', port=8080)
+    uvicorn.run('main:app', host='0.0.0.0', port=8080)
+    # uvicorn.run('main:app', host='localhost', port=8080)
