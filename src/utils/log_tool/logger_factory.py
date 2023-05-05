e4d90aace9468a420783782fd529b42e36f224c8
puffvayne <128754474+puffvayne@users.noreply.github.com>
2023-05-05 16:40:19 +0800

    Update logger_factory.py

--- src/utils/log_tool/logger_factory.py
+++ src/utils/log_tool/logger_factory.py
@@ -57,7 +57,7 @@ class CustomFormatter(logging.Formatter):
 
             hour_diff = (target_offset - server_offset).total_seconds() / 3600
             print(f"hour_diff = {hour_diff}")
-            
+
             record.created += hour_diff * 3600
             print(f"final created = {datetime.datetime.fromtimestamp(record.created)}")
 
