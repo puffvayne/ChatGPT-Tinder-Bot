4eaaa07a9947287827c089dc5f2f601575a3d570
puffvayne <128754474+puffvayne@users.noreply.github.com>
2023-07-29 19:08:21 +0800

    up

---
+++ src/utils/var/__init__.py
@@ -0,0 +1,7 @@
+import pathlib
+
+VAR_PKG_DIR = pathlib.Path(__file__).absolute().parent
+UTILS_PKG_DIR = VAR_PKG_DIR.parent
+PROJECT_ROOT_PKG_DIR = UTILS_PKG_DIR.parent
+PROJECT_DIR = PROJECT_ROOT_PKG_DIR.parent
+
